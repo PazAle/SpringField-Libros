4161fdd9c1f2deedfb6716530fa87de2970f0799
gabrieldp6880 <ivandp6880@gmail.com>
2023-10-07 13:54:01 -0300

    Ajuste sobre HibernateConfig

--- src/main/java/com/tallerwebi/config/HibernateConfig.java
+++ src/main/java/com/tallerwebi/config/HibernateConfig.java
@@ -38,6 +38,17 @@ public class HibernateConfig {
         return dataSource;
     }
 
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/db_applibros");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("root");
+//        return dataSource;
+//    }
+
+
     @Bean
     public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
@@ -54,10 +65,12 @@ public class HibernateConfig {
 
     private Properties hibernateProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
         properties.setProperty("hibernate.show_sql", "true");
         properties.setProperty("hibernate.format_sql", "true");
         properties.setProperty("hibernate.hbm2ddl.auto", "create");
         return properties;
     }
 }
+
+
